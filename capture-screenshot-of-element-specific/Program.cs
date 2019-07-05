d75091e73fd197fce22e7e874442b9e938ee4ea2
Paulo Henrique <mensa.geiro@hotmail.com>
2019-07-05 08:56:54 -0300

    Add project files.

---
+++ capture-screenshot-of-element-specific/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace capture_screenshot_of_element_specific
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
